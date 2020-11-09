2457be4b20f60a80995c205faa49eccaf5ac36b9
Cara Tiller <ctiller24@biomedscienceacademy.org>
2020-11-09 20:02:59 +0000

    I added the formatting.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Cara Tiller 
+Period/Core: 2
 
 
 """
+print("Welcome!  ")
+name = input("Please input a name: ")
+place = input("Please input a noun(place): ")
+coloree = input("Please input a adjective(color):  ")
+noun = input("Please input a noun(thing): ")
+number = input("Please input a number above 1: ")
+food = input("Please input a food: ")
+name2 = input("Please input another name: ")
+adjective = input("Please input a adjective(shape): ")
+adjective2 = input("Please input another adjective(texture): ")
+
+print(name + " was on their way to store when they noticed "+ number + " " + noun + "s.\n The " + noun +  "s were a nice shade of " + coloree + " ." )
+print(f"At the store {name} was looking at their shopping list.\n They needed {adjective2} {food}.\n Walking back to their house they stoped to say hi to their friend {name2} at the {place}.\n {name2} was wearing a {coloree} shirt with a {adjective} in the middle of it. \n They talked about the {adjective2} {food} and the weather.")
